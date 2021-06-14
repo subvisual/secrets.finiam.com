4933342d5363a20ac4dfdc958b6050529870c0f7
Francisco Sousa <francisco.sousa@hey.com>
2021-06-14 22:53:08 +0100

    Add favicon

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -3,6 +3,10 @@
   import '../app.css';
 </script>
 
+<svelte:head>
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+</svelte:head>
+
 <main class="max-w-2xl mx-auto pt-16 pb-6 flex flex-col items-center">
   <FiniamLogo class="mb-20" />
 
