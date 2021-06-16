2ace06ccddc4aa7a8a703a121f75f150eb0d415f
Francisco Sousa <josesousa9000@gmail.com>
2021-06-16 15:28:44 +0100

    Add link to repo and readme (#16)
    Add link to repo on the footer.
    Tidy up the readme for the public.

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -7,8 +7,22 @@
   <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 </svelte:head>
 
-<main class="max-w-2xl mx-auto pt-16 pb-6 flex flex-col items-center">
-  <FiniamLogo class="mb-20" />
+<div class="flex flex-col max-w-2xl min-h-screen mx-auto">
+  <main class="w-full pt-16 pb-6 flex flex-col items-center">
+    <FiniamLogo class="mb-20" />
 
-  <slot />
-</main>
+    <slot />
+  </main>
+
+  <footer class="mt-auto">
+    <p class="block my-20 mx-auto text-center">
+      Open source project. <a
+        class="underline"
+        href="https://github.com/finiam/secrets.finiam.com"
+        target="_blank"
+        rel="noopener"
+        >Check the repository here
+      </a>
+    </p>
+  </footer>
+</div>
