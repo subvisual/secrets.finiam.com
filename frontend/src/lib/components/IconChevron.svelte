b2860906250c26c976b0d8e002d8197a4d906817
Paula Carneiro <paulacarneiro215@gmail.com>
2022-07-12 15:28:39 +0100

    Add toogleable information paragraph

---
+++ frontend/src/lib/components/IconChevron.svelte
@@ -0,0 +1,16 @@
+<svg
+  width="15px"
+  height="15px"
+  viewBox="0 0 15 15"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  class={$$props.class}
+>
+  <path
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="1.25"
+    d="m10.625 6.25 -3.125 3.125 -3.125 -3.125"
+  />
+</svg>
--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,6 +1,5 @@
 <script context="module" lang="ts">
   import type { Load } from '@sveltejs/kit';
-  import Button from '$lib/components/Button.svelte';
   import { getStats } from '$lib/api';
   export const load: Load = async () => {
     const stats = await getStats();
@@ -13,14 +12,15 @@
 
 <script lang="ts">
   import SecretsLogo from '$lib/components/SecretsLogo.svelte';
+  import IconChevron from '$lib/components/IconChevron.svelte';
   import FiniamI from '$lib/components/FiniamI.svelte';
   import FiniamM from '$lib/components/FiniamM.svelte';
   import '../app.css';
 
   export let secretsCounter: number;
 
-  let isPopupOpen = false;
-  const togglePopup = () => (isPopupOpen = !isPopupOpen);
+  let toggleOpen = false;
+  const toggleInfo = () => (toggleOpen = !toggleOpen);
 </script>
 
 <svelte:head>
@@ -37,41 +37,62 @@
     <FiniamM class="absolute -right-8" />
   </div>
 
-  <div class="mx-auto flex min-h-screen w-full flex-col py-6 sm:w-5/6 md:pt-10">
-    <main class="mb-16 flex w-full grow flex-col items-center md:mb-4">
+  <div
+    class="mx-auto flex min-h-screen w-full flex-col justify-between py-6 sm:w-5/6 md:gap-6 md:pt-8"
+  >
+    <main class="mb-16 flex w-full flex-col items-center md:mb-3">
       <a href="/" class="outline-dark-grey">
-        <SecretsLogo class="mb-6 w-36 md:mb-10 md:w-48" />
+        <SecretsLogo class="fill-green mb-6 w-36 md:mb-6 md:w-48" />
       </a>
       <slot />
     </main>
 
     <footer
-      class="m-auto flex w-full flex-col items-center justify-around gap-4 px-5 tracking-tight md:flex-row md:gap-12"
+      class="mx-auto grid w-full grid-flow-row grid-cols-none gap-6 self-end justify-self-end px-5 tracking-tight md:grid-cols-2 md:grid-rows-none"
     >
-      <p class="text-center text-sm decoration-1">
-        <a
-          class="outline-dark-grey hover:text-green underline"
-          href="https://github.com/finiam/secrets.finiam.com"
-          target="_blank"
-          rel="noopener"
-          >Open source project
-        </a>
-        powered by
-        <a
-          class="outline-dark-grey hover:text-green font-medium"
-          href="https://finiam.com/"
-          target="_blank"
-          rel="noreferrer"
-          >Finiam
-        </a>
-      </p>
+      <div class="flex flex-col {toggleOpen ? 'self-start' : 'self-end'}">
+        <button on:click={toggleInfo} class="mx-auto flex items-center gap-1 text-sm"
+          >How it works <IconChevron class="{toggleOpen && 'rotate-180'} transition-all " />
+        </button>
 
-      <p class="flex items-center justify-center gap-2 text-center text-sm md:flex-row md:gap-3">
-        Secrets created so far:
-        <span class="bg-pink inline-flex h-12 w-12 items-center justify-center rounded-full">
+        <p
+          id="info"
+          class="{toggleOpen
+            ? 'block'
+            : 'hidden'}  mx-auto my-3 w-4/5 text-justify text-xs sm:text-xs"
+        >
+          Finiam Secrets transmits E2E messages safely by encrypting the user info locally and then
+          generating a URL with a private key embbeded on it, through the hash in the URL, which is
+          never sent to servers by browsers or any HTTP client. When you generate a secret, the
+          webapp posts the encrypted information to our API, which in turn stores that encrypted
+          information. Each secret can only be opened once - secrets get destroyed after they have
+          been opened or after they expire.
+        </p>
+      </div>
+
+      <div class="flex flex-col gap-3 sm:relative">
+        <p class="flex items-center justify-center gap-1 text-center text-xs md:flex-row md:gap-2">
+          Secrets created so far:
           {secretsCounter}
-        </span>
-      </p>
+        </p>
+        <p class="text-center text-xs decoration-1">
+          <a
+            class="outline-dark-grey hover:text-green underline underline-offset-1 "
+            href="https://github.com/finiam/secrets.finiam.com"
+            target="_blank"
+            rel="noopener"
+            >Open source project
+          </a>
+          powered by
+          <a
+            class="outline-dark-grey hover:text-green font-medium"
+            href="https://finiam.com/"
+            target="_blank"
+            rel="noreferrer"
+            >Finiam
+          </a>
+        </p>
+      </div>
     </footer>
   </div>
 </div>
